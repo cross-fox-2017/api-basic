9c92e51ad4b603687423304bd52883c97a19c166
Syanmil <syanmil@yahoo.com>
2017-01-30 14:21:38 +0700

    initialize server and models

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # api-basic
+
+Route | HTTP | Description
+--- | --- | ---
+/api/users | GET | Get all the users
+/api/users/:id | GET | Get single user
+/api/users | POST | Create user
+/api/users/:id | DELETE | Delete user
+/api/users/:id | PUT | Update a user with new info
---
+++ seeders/20170130071711-initial_seed.js
@@ -0,0 +1,36 @@
+'use strict';
+const faker = require('faker')
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('users', [
+      {name: faker.name.findName(), email: faker.internet.email(), address: faker.address.city(), createdAt: new Date(), updatedAt: new Date()},
+      {name: faker.name.findName(), email: faker.internet.email(), address: faker.address.city(), createdAt: new Date(), updatedAt: new Date()},
+      {name: faker.name.findName(), email: faker.internet.email(), address: faker.address.city(), createdAt: new Date(), updatedAt: new Date()},
+      {name: faker.name.findName(), email: faker.internet.email(), address: faker.address.city(), createdAt: new Date(), updatedAt: new Date()},
+      {name: faker.name.findName(), email: faker.internet.email(), address: faker.address.city(), createdAt: new Date(), updatedAt: new Date()},
+    ], {});
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+    return queryInterface.bulkDelete('users', null, {});
+
+  }
+};
