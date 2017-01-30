23affed22820f53f3a2c89ee06c95bdf37975569
Didit Suryadi <email": null>
2017-01-30 16:11:34 +0700

    release 0 1 2 3 done

---
+++ api/users.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+var controller = require('./controller/controllerUser')
+/* GET home page. */
+router.get('/', );
+
+
+module.exports = router;
