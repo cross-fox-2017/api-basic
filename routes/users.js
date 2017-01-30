c23dc552920029893d4b2ac7df8b4a2ead8d7747
qblol <iqbal.0809@gmail.com>
2017-01-30 14:34:03 +0700

    add routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+const models = require('../../models');
+var user = models.User;
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/api/users', function(req, res) {
+  user.findAll().then(function(result){
+    res.render('index', {result: result, title: "User List"})
+  })
+});
+
+router.get('api/users/:id',function(req,res){
+  user.findById(req.params.id).then(function(use){
+    res.render('index', {result: result})
+    })
+});
+
+router.post('/api/users', function(req, res) {
+  user.create({username: req.body.username, password: req.body.password, createdAt: new Date(), updatedAt: new Date()}).then(function(result){
+    res.redirect('/')
+  })
+});
+
+router.delete('api/users/:id',function(req,res){
+  user.findById(req.params.id).then(function(use){
+    use.destroy()
+    res.redirect('/')
+    })
+});
+
+router.put('api/users/:id',function(req,res){
+  user.findById(req.body.id).then(function(use){
+    use.update({username:req.body.username,password:req.body.password,updatedAt: new Date()})
+    res.redirect('/')
+    })
 });
 
 module.exports = router;
